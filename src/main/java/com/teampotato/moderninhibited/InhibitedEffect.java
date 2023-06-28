91900b92659feeb74e637aee2345d16aa4f64ba9
Kasualix <670067575@qq.com>
2023-06-28 15:31:29 +0800

    complete

--- src/main/java/com/teampotato/moderninhibited/InhibitedEffect.java
+++ src/main/java/com/teampotato/moderninhibited/InhibitedEffect.java
@@ -1,11 +1,7 @@
 package com.teampotato.moderninhibited;
 
-import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.level.GameType;
 
 import javax.annotation.ParametersAreNonnullByDefault;
 
@@ -13,16 +9,6 @@ import javax.annotation.ParametersAreNonnullByDefault;
 public class InhibitedEffect extends MobEffect {
     protected InhibitedEffect() {
         super(MobEffectCategory.HARMFUL, 16711680);
-    }
-
-    public void applyEffectTick(LivingEntity pLivingEntity, int pAmplifier) {
-        if (pLivingEntity instanceof Player && !((ServerPlayer) pLivingEntity).isCreative() && !pLivingEntity.isSpectator()) {
-            ((ServerPlayer) pLivingEntity).setGameMode(GameType.ADVENTURE);
-        }
-    }
-
-    public boolean isDurationEffectTick(int pDuration, int pAmplifier) {
-        return pDuration > 0;
     }
 
     public boolean isBeneficial() {
--- src/main/java/com/teampotato/moderninhibited/ModernInhibited.java
+++ src/main/java/com/teampotato/moderninhibited/ModernInhibited.java
@@ -1,12 +1,6 @@
 package com.teampotato.moderninhibited;
 
-import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.effect.MobEffect;
-import net.minecraft.world.effect.MobEffectInstance;
-import net.minecraft.world.level.GameType;
-import net.minecraftforge.event.entity.living.MobEffectEvent;
-import net.minecraftforge.eventbus.api.EventPriority;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
@@ -16,23 +10,11 @@ import net.minecraftforge.registries.RegistryObject;
 @Mod(ModernInhibited.ID)
 public class ModernInhibited {
     public static final String ID = "moderninhibited";
-    public static final DeferredRegister<MobEffect> EFFECT_DEFERRED_REGISTER = DeferredRegister.create(ForgeRegistries.MOB_EFFECTS, "cataclysm");
+    public static final DeferredRegister<MobEffect> EFFECT_DEFERRED_REGISTER = DeferredRegister.create(ForgeRegistries.MOB_EFFECTS, ID);
 
     @SuppressWarnings("unused") public static final RegistryObject<MobEffect> INHIBITED =  EFFECT_DEFERRED_REGISTER.register("inhibited", InhibitedEffect::new);
 
     public ModernInhibited() {
         EFFECT_DEFERRED_REGISTER.register(FMLJavaModLoadingContext.get().getModEventBus());
-    }
-
-    @SubscribeEvent(priority = EventPriority.HIGHEST)
-    public static void onExpirePotion(MobEffectEvent.Expired event) {
-        MobEffectInstance effectInstance = event.getEffectInstance();
-        if (effectInstance != null && effectInstance.getEffect().equals(INHIBITED.get()) &&
-                event.getEntity() instanceof ServerPlayer &&
-                !((ServerPlayer) event.getEntity()).isCreative() &&
-                !event.getEntity().isSpectator()
-        ) {
-            ((ServerPlayer) event.getEntity()).setGameMode(GameType.SURVIVAL);
-        }
     }
 }
