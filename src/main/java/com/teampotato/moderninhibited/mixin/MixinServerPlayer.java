3741e9cca85249315e9b016ed42a1289da8792c2
Kasualix <670067575@qq.com>
2023-08-23 13:39:25 +0800

    1182

--- src/main/java/com/teampotato/moderninhibited/mixin/MixinServerPlayer.java
+++ src/main/java/com/teampotato/moderninhibited/mixin/MixinServerPlayer.java
@@ -6,10 +6,8 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.level.ServerPlayerGameMode;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.entity.player.ProfilePublicKey;
 import net.minecraft.world.level.GameType;
 import net.minecraft.world.level.Level;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -19,14 +17,15 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ServerPlayer.class)
 public abstract class MixinServerPlayer extends Player {
+    public MixinServerPlayer(Level pLevel, BlockPos pPos, float pYRot, GameProfile pGameProfile) {
+        super(pLevel, pPos, pYRot, pGameProfile);
+    }
+
     @Shadow public abstract boolean isCreative();
     @Shadow public abstract boolean isSpectator();
     @Shadow public abstract boolean setGameMode(GameType pGameMode);
     @Shadow @Final public ServerPlayerGameMode gameMode;
 
-    public MixinServerPlayer(Level pLevel, BlockPos pPos, float pYRot, GameProfile pGameProfile, @Nullable ProfilePublicKey pProfilePublicKey) {
-        super(pLevel, pPos, pYRot, pGameProfile, pProfilePublicKey);
-    }
 
     @Inject(method = "tick", at = @At("HEAD"))
     private void onTick(CallbackInfo ci) {
