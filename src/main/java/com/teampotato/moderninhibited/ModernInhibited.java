29d20f316e94e71dbb998d101f1698d3d5f49a6e
Kasualix <670067575@qq.com>
2023-09-25 21:55:38 +0800

    1.0.1

--- src/main/java/com/teampotato/moderninhibited/ModernInhibited.java
+++ src/main/java/com/teampotato/moderninhibited/ModernInhibited.java
@@ -1,11 +1,17 @@
 package com.teampotato.moderninhibited;
 
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import net.minecraft.world.effect.MobEffect;
+import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
+
+import java.util.List;
 
 @Mod(ModernInhibited.ID)
 public class ModernInhibited {
@@ -16,5 +22,17 @@ public class ModernInhibited {
 
     public ModernInhibited() {
         EFFECT_DEFERRED_REGISTER.register(FMLJavaModLoadingContext.get().getModEventBus());
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, config);
+    }
+
+    public static final ForgeConfigSpec config;
+    public static final ForgeConfigSpec.ConfigValue<List<? extends String>> validStructures;
+
+    static {
+        ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
+        builder.push("ModernInhibited");
+        validStructures = builder.defineList("validStructures", new ObjectArrayList<>(), o -> true);
+        builder.pop();
+        config = builder.build();
     }
 }
