c0b05b09106bffc4f168ca2b7ef40451724e09ee
Kasualix <670067575@qq.com>
2023-09-26 12:44:40 +0800

    1201

--- src/main/java/com/teampotato/moderninhibited/mixin/MixinPlayer.java
+++ src/main/java/com/teampotato/moderninhibited/mixin/MixinPlayer.java
@@ -2,7 +2,7 @@ package com.teampotato.moderninhibited.mixin;
 
 import com.teampotato.moderninhibited.ModernInhibited;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.effect.MobEffectInstance;
@@ -37,12 +37,12 @@ public abstract class MixinPlayer extends LivingEntity {
         mi$tickCount++;
         if (this.hasEffect(ModernInhibited.INHIBITED.get()) || this.isSpectator() || this.isCreative() || mi$tickCount % 40 != 0) return;
         BlockPos blockPosition = this.blockPosition();
-        ChunkAccess chunkAccess = this.level.getChunkAt(blockPosition);
+        ChunkAccess chunkAccess = this.level().getChunkAt(blockPosition);
         chunkAccess.getAllReferences().keySet().forEach(structure -> {
-            if (this.level instanceof ServerLevel serverLevel) {
+            if (this.level() instanceof ServerLevel serverLevel) {
                 StructureStart structureStart = serverLevel.structureManager().getStructureAt(blockPosition, structure);
                 if (!structureStart.equals(StructureStart.INVALID_START)) {
-                    ResourceLocation id = Registry.STRUCTURE_TYPES.getKey(structure.type());
+                    ResourceLocation id = BuiltInRegistries.STRUCTURE_TYPE.getKey(structure.type());
                     if (id != null && ModernInhibited.validStructures.get().contains(id.toString())) {
                         this.addEffect(new MobEffectInstance(ModernInhibited.INHIBITED.get(), 200));
                     }
