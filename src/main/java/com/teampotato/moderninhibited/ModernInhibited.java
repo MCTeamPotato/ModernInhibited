5e2db0fcea6178ad6b455c7f7619282641fa2325
Kasualix <670067575@qq.com>
2023-10-04 17:10:46 +0800

    1.0.3

--- src/main/java/com/teampotato/moderninhibited/ModernInhibited.java
+++ src/main/java/com/teampotato/moderninhibited/ModernInhibited.java
@@ -1,20 +1,63 @@
 package com.teampotato.moderninhibited;
 
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.level.GameType;
+import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.event.entity.living.PotionEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
 
 @Mod(ModernInhibited.ID)
 public class ModernInhibited {
     public static final String ID = "moderninhibited";
     public static final DeferredRegister<MobEffect> EFFECT_DEFERRED_REGISTER = DeferredRegister.create(ForgeRegistries.MOB_EFFECTS, ID);
 
-    @SuppressWarnings("unused") public static final RegistryObject<MobEffect> INHIBITED =  EFFECT_DEFERRED_REGISTER.register("inhibited", InhibitedEffect::new);
+    public static final RegistryObject<MobEffect> INHIBITED = EFFECT_DEFERRED_REGISTER.register("inhibited", InhibitedEffect::new);
 
     public ModernInhibited() {
         EFFECT_DEFERRED_REGISTER.register(FMLJavaModLoadingContext.get().getModEventBus());
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, config);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    public static final ForgeConfigSpec config;
+    public static final ForgeConfigSpec.ConfigValue<List<? extends String>> validStructures;
+
+    static {
+        ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
+        builder.push("ModernInhibited");
+        validStructures = builder.defineList("validStructures", new ObjectArrayList<>(), o -> true);
+        builder.pop();
+        config = builder.build();
+    }
+
+    @SubscribeEvent
+    public void onServerPlayerTick(TickEvent.@NotNull PlayerTickEvent event) {
+        if (event.phase == TickEvent.Phase.START && event.player instanceof ServerPlayer serverPlayer) {
+            if (serverPlayer.isCreative() || serverPlayer.isSpectator()) return;
+            if (serverPlayer.hasEffect(ModernInhibited.INHIBITED.get()) && serverPlayer.gameMode.getGameModeForPlayer() == GameType.SURVIVAL) {
+                serverPlayer.setGameMode(GameType.ADVENTURE);
+            }
+        }
+    }
+
+    @SubscribeEvent
+    public void onPotionExpired(PotionEvent.PotionExpiryEvent event) {
+        if (event.getPotionEffect() != null && event.getPotionEffect().getEffect().equals(INHIBITED.get()) && event.getEntity() instanceof ServerPlayer serverPlayer) {
+            serverPlayer.setGameMode(GameType.SURVIVAL);
+        }
     }
 }
---
+++ src/main/java/com/teampotato/moderninhibited/mixin/MixinPlayer.java
@@ -0,0 +1,51 @@
+package com.teampotato.moderninhibited.mixin;
+
+import com.teampotato.moderninhibited.ModernInhibited;
+import net.minecraft.core.BlockPos;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.chunk.LevelChunk;
+import net.minecraft.world.level.levelgen.feature.ConfiguredStructureFeature;
+import net.minecraft.world.level.levelgen.structure.StructureStart;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Player.class)
+public abstract class MixinPlayer extends LivingEntity {
+    @Shadow public abstract boolean isSpectator();
+
+    @Shadow public abstract boolean isCreative();
+
+    @Unique
+    private int mi$tickCount;
+
+    protected MixinPlayer(EntityType<? extends LivingEntity> pEntityType, Level pLevel) {
+        super(pEntityType, pLevel);
+    }
+
+    @Inject(method = "tick", at = @At(value = "INVOKE", remap = false, target = "Lnet/minecraftforge/event/ForgeEventFactory;onPlayerPreTick(Lnet/minecraft/world/entity/player/Player;)V", shift = At.Shift.AFTER))
+    private void onTick(CallbackInfo ci) {
+        mi$tickCount++;
+        if (this.hasEffect(ModernInhibited.INHIBITED.get()) || this.isSpectator() || this.isCreative() || mi$tickCount % 40 != 0) return;
+        BlockPos blockPosition = this.blockPosition();
+        LevelChunk levelChunk = this.level.getChunkAt(blockPosition);
+        for (ConfiguredStructureFeature<?, ?> structure : levelChunk.getAllReferences().keySet()) {
+            StructureStart structureStart = levelChunk.getStartForFeature(structure);
+            if (structureStart != null && structureStart.getBoundingBox().isInside(this.blockPosition())) {
+                ResourceLocation id = structure.feature.getRegistryName();
+                if (id != null && ModernInhibited.validStructures.get().contains(id.toString())) {
+                    this.addEffect(new MobEffectInstance(ModernInhibited.INHIBITED.get(), 200));
+                    break;
+                }
+            }
+        }
+    }
+}
